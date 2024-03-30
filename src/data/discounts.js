58fc0eaf954301461f825fb18bc9d454430bfb61
Fermin Rodriguez Penelas <frodriguezpenelas@gmail.com>
2024-03-30 17:25:25 -0300

    ordered discounts

--- src/data/discounts.js
+++ src/data/discounts.js
@@ -1,34 +1,27 @@
 const discounts = {
-    Lunes : [
-      {
-        estacion: 'Axion',
-        cuenta: 'Modo + BBVA',
-        descuento: '30%',
-        tope: '$2,500',
-        condiciones: [
-          'Solo valido en Infinia',
-          'Solo en Provincia'
-        ]
-      }
-    ],
-    Martes : [],
-    Miercoles : [],
-    Jueves: [],
-    Viernes: [],
-    'Sábado' : [],
-    Domingo : [
-      {
-        estacion: 'Axion',
-        cuenta: 'Modo + BBVA',
-        descuento: '30%',
-        tope: '$2,500',
-        condiciones: [
-          'Solo valido en Infinia',
-          'Solo en Provincia'
-        ]
-      }
-    ]
-   }
-   
-   export default discounts;
-   
+	Lunes: [
+		{
+			estacion: 'Axion',
+			cuenta: 'Modo + BBVA',
+			descuento: '30%',
+			tope: '$2,500',
+			condiciones: ['Solo valido en Infinia', 'Solo en Provincia']
+		}
+	],
+	Martes: [],
+	Miércoles: [],
+	Jueves: [],
+	Viernes: [],
+	Sábado: [],
+	Domingo: [
+		{
+			estacion: 'Axion',
+			cuenta: 'Modo + BBVA',
+			descuento: '30%',
+			tope: '$2,500',
+			condiciones: ['Solo valido en Infinia', 'Solo en Provincia']
+		}
+	]
+};
+
+export default discounts;
--- src/data/orderedDiscounts.js
+++ src/data/orderedDiscounts.js
@@ -1,14 +1,28 @@
 import discounts from './discounts'; // Asegúrate de que tu ruta de importación sea correcta
+// console.log(discounts);
+
 
 const getDayName = (date, locale) => {
   let dayName = date.toLocaleDateString(locale, { weekday: 'long' });
-  return dayName.charAt(0).toUpperCase() + dayName.slice(1); // Asegurándonos de que la primera letra sea mayúscula
+  return dayName.charAt(0).toUpperCase() + dayName.slice(1); 
 }
 
 const getTomorrowName = (date, locale) => {
   let tomorrow = new Date(date.getTime()); 
   tomorrow.setDate(tomorrow.getDate() + 1); 
-  return getDayName(tomorrow, locale);
+  return dayName.charAt(0).toUpperCase() + dayName.slice(1); 
+}
+
+const getOrderedWeekdays = (startDay) => {
+  const weekdays = ["Domingo", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado"];
+  let startIndex = weekdays.indexOf(startDay);
+  if (startIndex === -1) return weekdays; // Si el día de inicio no se encuentra, devuelve los días de la semana en el orden original
+
+  let orderedWeekdays = [];
+  for (let i = 0; i < weekdays.length; i++) {
+    orderedWeekdays.push(weekdays[(startIndex + i) % weekdays.length]);
+  }
+  return orderedWeekdays;
 }
 
 // Obteniendo el nombre del día actual de la semana en español
@@ -18,20 +32,19 @@ const dayName = getDayName(new Date(), "es-ES");
 const tomorrowName = getTomorrowName(new Date(), "es-ES");
 
 const orderDiscounts = (discounts, dayName, tomorrowName) => {
-  const ordered = {}
-  ordered["Hoy"] = discounts[dayName];
+  const ordered = {};
+  const orderedWeekdays = getOrderedWeekdays(tomorrowName); // Obtén los días de la semana ordenados a partir de mañana
 
-  // Comprueba si existe un descuento para "Mañana"
-  if (discounts[tomorrowName]) {
-    ordered["Mañana"] = discounts[tomorrowName];
-  }
-  
-  for (let day in discounts) {
+  ordered["Hoy"] = discounts[dayName];
+  ordered["Mañana"] = discounts[tomorrowName];
+
+  // Ordenar los días restantes según el orden lógico después de mañana
+  orderedWeekdays.forEach(day => {
     if (day !== dayName && day !== tomorrowName) {
       ordered[day] = discounts[day];
     }
-  }
-  
+  });
+
   return ordered;
 }
 
